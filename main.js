e9717c2c2e09ac2fd173cf44136dcd337e993324
Iskandar780 <85502205+Iskandar780@users.noreply.github.com>
2021-07-26 18:44:52 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+
+//var user_name = ' iskandar ';
+//var user_age = ' 24 ';
+//console.log(' ismim ' + user_name + ' yoshim esa ' + user_age + ' da ');
+//var user_name = prompt('ismingizni kiriting');
+//var user_born = prompt('tugulgan yilingizni kiriting');
+//ar user_age = 2021- user_born;
+//lert(' mani ismim ' +  user_name  + ' ' + user_age + ' yoshdaman ');
+
+//uy ishi
+var user_name = prompt(' Ismingizni kiriting ');
+console.log(' mening ismim ' + user_name );
+
+var user_age = prompt(' Yoshingizni kiriting ');
+console.log(' mening yoshim ' + user_age + ' da ');
+
+var misol_1 = prompt( ' misolni yeching 6 + 2 = ?');
+var misol1 = prompt('6 + 2 =8');
+console.log(' siznig javobingiz ' +  misol_1 + misol1);
+
+var misol_2 = prompt( ' misolni yeching 20 - 5 = ?');
+console.log(' siznig javobingiz '  +  misol_2 );
+
+var misol_3 = prompt( ' misolni yeching 15 * 2 = ?');
+console.log(' siznig javobingiz '  +  misol_3 );
+
+var misol_4 = prompt( ' misolni yeching 8 / 4 = ?');
+console.log(' siznig javobingiz '  +  misol_4 );
+
+var misol_5 = prompt( ' misolni yeching 10 % 3 = ?');
+console.log(' siznig javobingiz '  +  misol_5 );
+alert(' natijani bilish uchun bosing ');
